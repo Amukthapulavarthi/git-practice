98f3f02903db9b9a9d6ea64e7c7b0d88437971c2
amukthapulavarthi <pulavarthiamuktha@gmail.com>
2025-03-15 18:35:32 +0530

    variables

---
+++ 05-Array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Fruits=("Apple" "Kiwi" "Chiko")
+
+echo "First fruit is ${Fruits[0]}"
+echo "second fruit is ${Fruits[1]}"
+echo  "Third fruit is ${Fruits[2]}"
+echo "All fruits are ${Fruits[@]}"
