09c6881ac0d46701f3e2eb4ec1398d5851f8e388
amukthapulavarthi <pulavarthiamuktha@gmail.com>
2025-03-17 12:32:14 +0530

    duplicate file

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -5,4 +5,6 @@ echo "No.of variables passed to the script : $#"
 echo "script name : $0"
 echo "current working directory : $PWD"
 echo "Home directory of current user : $HOME"
+echo "PID of current script : $$"
+
 
