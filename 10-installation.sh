d0f0113e2f88d3b37d30199288638860ce5e0e61
amukthapulavarthi <pulavarthiamuktha@gmail.com>
2025-03-17 13:56:27 +0530

    duplicate file

--- 10-installation.sh
+++ 10-installation.sh
@@ -12,13 +12,13 @@ dnf list installed git
  if [ $? -ne 0 ]
   then
     echo "git is not there ... please install"
-    dnf install git -y
-     if [ $? -ne 0 ]
-     then
-      echo "git is not installed .. please check"
-      exit 1
+     dnf install git -y
+      if [ $? -ne 0 ]
+      then
+       echo "git is not installed .. please check"
+       exit 1
      else
-   echo "git installation success"
+    echo "git installation success"
  fi
  else
    echo "git is already installed... nothing to do"
